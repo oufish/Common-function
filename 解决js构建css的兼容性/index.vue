ba61e1e62692c61b8e3b490bf9eb68a95fa511aa
oufish <674106992@qq.com>
2018-01-16 11:28:21 +0800

    解决js构建css的兼容

---
+++ 解决js构建css的兼容性/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>解决使用js构建的css兼容性问题</p>
+    <p ref="demo">修改我的颜色</p>
+</template>
+
+<script>
+    import {prefixStyle} from './prefixStyle'
+    const transform = prefixStyle('transform')
+    export default {
+        name: 'prefixStyle',
+        methods: {
+            _changeStyle() {
+                this.$refs.demo.style[transform] = `translate3d(x,x,x) scale(x)`
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 解决js构建css的兼容性/prefixStyle.js
@@ -0,0 +1,35 @@
+/**
+ * 解决使用js构建的css兼容性问题
+ * auth  oufish 
+ * @param {string} style    ：transform
+ */
+let elementStyle = document.createElement('div').style
+let vendor = (() => {
+    let transformNames = {
+        webkit: 'webkitTransform',
+        Moz: 'MozTransform',
+        O: 'OTransform',
+        ms: 'msTransform',
+        standard: 'transform'
+    }
+
+    for (let key in transformNames) {
+        if (elementStyle[transformNames[key]] !== undefined) {
+            return key
+        }
+    }
+
+    return false
+})()
+
+export function prefixStyle(style) {
+    if (vendor === false) {
+        return false
+    }
+
+    if (vendor === 'standard') {
+        return style
+    }
+
+    return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
